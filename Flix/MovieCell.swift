45e1828ca6f5125b49d68969de0e7e198bcac0c5
bryanr1992 <bryan.rivas05@gmail.com>
2019-10-16 23:02:14 -0400

    Finished all required features

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Bryan Rivas on 10/16/19.
+//  Copyright © 2019 Bryan Rivas. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterDisplay: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
    
@@ -49,13 +50,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return movies.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
-        
         let title = movie["original_title"] as! String
+        let synopsis = movie["overview"] as! String
+
+        cell.titleLabel!.text = title
+        cell.synopsisLabel!.text = synopsis
         
-        cell.textLabel!.text = title
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterDisplay.af_setImage(withURL: posterURL!)
         
         return cell
     }
