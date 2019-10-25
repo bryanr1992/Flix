316d4b77c141680212b3cda2364b84bf7c5d4f24
bryanr1992 <bryan.rivas05@gmail.com>
2019-10-24 22:33:43 -0400

    Add tab bar controller, a new view controller for the second tab bar option  and icons for each tab.

---
+++ Flix/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Bryan Rivas on 10/24/19.
+//  Copyright © 2019 Bryan Rivas. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
